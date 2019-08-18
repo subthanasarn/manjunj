3ff22e54f9fec0e9d8694023eb3c9d097f656c98
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2019-08-19 00:12:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -392,17 +392,64 @@ fi
 
 #install Nginx
 ok "➡ apt-get install nginx"
-apt-get install -qy nginx > /dev/null 2>&1
-rm -f /etc/nginx/sites-enabled/default
-rm -f /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "$zenon/nginx.conf"
-wget -q -O /etc/nginx/conf.d/vps.conf "$zenon/vps.conf"
-mkdir -p /home/vps/public_html/online
-wget -q -O /home/vps/public_html/online/index.php "$zenon/2.txt"
-wget -q -O /home/vps/public_html/index.html "$zenon/1.txt"
-echo "<?php phpinfo( ); ?>" > /home/vps/public_html/info.php
+	apt-get -y install nginx
+	cat > /etc/nginx/nginx.conf <<END
+user www-data;
+worker_processes 2;
+pid /var/run/nginx.pid;
+events {
+	multi_accept on;
+        worker_connections 1024;
+}
+http {
+	autoindex on;
+        sendfile on;
+        tcp_nopush on;
+        tcp_nodelay on;
+        keepalive_timeout 65;
+        types_hash_max_size 2048;
+        server_tokens off;
+        include /etc/nginx/mime.types;
+        default_type application/octet-stream;
+        access_log /var/log/nginx/access.log;
+        error_log /var/log/nginx/error.log;
+        client_max_body_size 32M;
+	client_header_buffer_size 8m;
+	large_client_header_buffers 8 8m;
+	fastcgi_buffer_size 8m;
+	fastcgi_buffers 8 8m;
+	fastcgi_read_timeout 600;
+        include /etc/nginx/conf.d/*.conf;
+}
+END
+	mkdir -p /home/vps/public_html
+	echo "<pre>Source by Mnm Ami | Donate via TrueMoney Walle 096-746-2879 </pre>" > /home/vps/public_html/index.html
+	echo "<?phpinfo(); ?>" > /home/vps/public_html/info.php
+	args='$args'
+	uri='$uri'
+	document_root='$document_root'
+	fastcgi_script_name='$fastcgi_script_name'
+	cat > /etc/nginx/conf.d/vps.conf <<END
+server {
+    listen       85;
+    server_name  127.0.0.1 localhost;
+    access_log /var/log/nginx/vps-access.log;
+    error_log /var/log/nginx/vps-error.log error;
+    root   /home/vps/public_html;
+    location / {
+        index  index.html index.htm index.php;
+	try_files $uri $uri/ /index.php?$args;
+    }
+    location ~ \.php$ {
+        include /etc/nginx/fastcgi_params;
+        fastcgi_pass  127.0.0.1:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    }
+}
+END
 ok "➡ service nginx restart"
-service nginx restart > /dev/null 2>&1
+service nginx restart 
 
 #install php-fpm
 if [[ "$VERSION_ID" = 'VERSION_ID="7"' || "$VERSION_ID" = 'VERSION_ID="8"' || "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
@@ -576,14 +623,14 @@ echo -e "\e[32m    #    Dropbear       :   22, 443                              
 echo -e "\e[32m    #    SSL            :   444                                  #    "
 echo -e "\e[32m    #    Squid3         :   3128, 8080                           #    "
 echo -e "\e[32m    #    OpenVPN        :   TCP 1194                             #    "
-echo -e "\e[32m    #    Nginx          :   80                                   #    "
+echo -e "\e[32m    #    Nginx          :   85                                  #    "
 echo -e "\e[32m    #                                                            #    "
 echo -e "\e[32m    #                 TOOLS VPN SSH                              #    "
 echo -e "\e[32m    #                                                            #    "
 echo -e "\e[32m    #    Useronline     :   http://$SERVER_IP/online         #    "
 echo -e "\e[32m    #    Vnstat         :   http://$SERVER_IP/bandwidth      #    "
 echo -e "\e[32m    #    Webmin         :   http://$SERVER_IP:10000          #    "
-echo -e "\e[32m    #    ConfigVPN      :   http://$SERVER_IP/zenon.ovpn     #    "
+echo -e "\e[32m    #    ConfigVPN      :   http://$SERVER_IP:85/$zenon.ovpn      #    "
 echo -e "\e[32m    #                                                            #    "
 echo -e "\e[32m    #             LIMIT SETTINGS VPN SSH                         #    "
 echo -e "\e[32m    #                                                            #    "
